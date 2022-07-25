3ece97860b4baf6b1b0845fd34937d33d7defdc1
kimmyoungji <kmjoyit21045@gmail.com>
2022-07-25 23:27:28 +0900

    copying
    copying teacher's git repository

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script lang="ts">
+	import { Button } from '@src/components/button';
+
+	const textNumbers: string[] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+</script>
+
+<div class="frame">
+	{#each textNumbers as text}
+		<Button {text} color="orange" onClick={(value) => alert(value)} />
+	{/each}
+</div>
+
+<style lang="scss">
+	.frame {
+		width: 240px;
+		display: flex;
+		flex-wrap: wrap;
+	}
+</style>
